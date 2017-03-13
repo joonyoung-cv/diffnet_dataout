2b124809dc737956c7f1f9cb36d0d3ffad68eb92
jolee <jolee@adobe.com>
2017-03-13 11:16:19 -0700

    fix link_best to save model_000.t7 instead of best.t7

--- link_best.py
+++ link_best.py
@@ -7,4 +7,4 @@ vals = vals.split()
 for val in vals:
     ep = subprocess.check_output("sort -rk3 {} | awk 'NR==1 {{print $1}}'".format(val), shell=True).strip()
     dirpath = os.path.dirname(val)
-    os.system("cd {} && ln -fs model_{}.t7 best.t7".format(dirpath, ep))
+    os.system("cd {} && ln -fs model_{}.t7 model_000.t7".format(dirpath, ep))
