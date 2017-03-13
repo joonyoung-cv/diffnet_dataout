188c971e490282b2c5f28c71bcf9622d7b7821ab
jolee <jolee@adobe.com>
2017-03-13 10:52:11 -0700

    link best val

---
+++ link_best.py
@@ -0,0 +1,10 @@
+import os
+import os.path
+import subprocess
+
+vals = subprocess.check_output("find . -name 'val.log'", shell=True)
+vals = vals.split()
+for val in vals:
+    ep = subprocess.check_output("sort -rk3 {} | awk 'NR==1 {{print $1}}'".format(val), shell=True).strip()
+    dirpath = os.path.dirname(val)
+    os.system("cd {} && ln -fs model_{}.t7 best.t7".format(dirpath, ep))
