dbddebf4adcab8a76ef884cd0ac0f182e4140779
jolee <jolee@adobe.com>
2017-03-15 09:49:46 -0700

    update

--- link_best.py
+++ link_best.py
@@ -5,6 +5,7 @@ import subprocess
 vals = subprocess.check_output("find . -name 'val.log'", shell=True)
 vals = vals.split()
 for val in vals:
-    ep = subprocess.check_output("sort -rk3 {} | awk 'NR==1 {{print $1}}'".format(val), shell=True).strip()
     dirpath = os.path.dirname(val)
+    os.system("sort -rk3 {} > {}/val_sorted.log".format(val, dirpath))
+    ep = subprocess.check_output("sort -rk3 {} | awk 'NR==1 {{print $1}}'".format(val), shell=True).strip()
     os.system("cd {} && ln -fs model_{}.t7 model_000.t7".format(dirpath, ep))
