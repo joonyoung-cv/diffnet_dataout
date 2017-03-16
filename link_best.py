43a8cfb0c92b229399ff906ea13a9b9900b1d81a
jolee <jolee@adobe.com>
2017-03-15 18:51:44 -0700

    update

--- link_best.py
+++ link_best.py
@@ -5,7 +5,9 @@ import subprocess
 vals = subprocess.check_output("find . -name 'val.log'", shell=True)
 vals = vals.split()
 for val in vals:
-    dirpath = os.path.dirname(val)
-    os.system("sort -rk3 {} > {}/val_sorted.log".format(val, dirpath))
-    ep = subprocess.check_output("sort -rk3 {} | awk 'NR==1 {{print $1}}'".format(val), shell=True).strip()
-    os.system("cd {} && ln -fs model_{}.t7 model_000.t7".format(dirpath, ep))
+    chk = subprocess.check_output("cat {} |wc -l".format(val), shell=True).strip()
+    if chk == '30':
+        dirpath = os.path.dirname(val)
+        os.system("sort -rk3 {} > {}/val_sorted.log".format(val, dirpath))
+        ep = subprocess.check_output("sort -rk3 {} | awk 'NR==1 {{print $1}}'".format(val), shell=True).strip()
+        os.system("cd {} && ln -fs model_{}.t7 model_000.t7".format(dirpath, ep))
