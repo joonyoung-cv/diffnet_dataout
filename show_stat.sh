276e9cd71135addba8e7d12eec9b2f6ce46dd2ef
jolee <jolee@adobe.com>
2017-03-17 11:01:11 -0700

    update

--- show_stat.sh
+++ show_stat.sh
@@ -1,20 +1,20 @@
-echo 'UCF MEAN TOP1'
-echo 'UCF-101 vggm_s' && grep "MEAN TOP" `find UCF101_RGB_S* -name test_nc25_1.log |grep vggmpt_s_ --color=never`
-echo 'UCF-101 vggm_d' && grep "MEAN TOP" `find UCF101_RGB_S* -name test_nc25_1.log |grep vggmpt_d_ --color=never`
-echo 'UCF-101 vggm_sdsel' && grep "MEAN TOP" `find UCF101_RGB_S* -name test_nc25_1.log |grep vggmpt_sdsel_ --color=never`
+#echo 'UCF MEAN TOP1'
+#echo 'UCF-101 vggm_s' && grep "MEAN TOP" `find UCF101_RGB_S* -name test_nc25_1.log |grep vggmpt_s_ --color=never`
+#echo 'UCF-101 vggm_d' && grep "MEAN TOP" `find UCF101_RGB_S* -name test_nc25_1.log |grep vggmpt_d_ --color=never`
+#echo 'UCF-101 vggm_sdsel' && grep "MEAN TOP" `find UCF101_RGB_S* -name test_nc25_1.log |grep vggmpt_sdsel_ --color=never`
 
-echo 'UCF-101 vgg16_s' && grep "MEAN TOP" `find UCF101_RGB_S* -name test_nc25_1.log |grep vgg16pt_s_ --color=never`
-echo 'UCF-101 vgg16_d' && grep "MEAN TOP" `find UCF101_RGB_S* -name test_nc25_1.log |grep vgg16pt_d_ --color=never`
-echo 'UCF-101 vgg16_sdsel' && grep "MEAN TOP" `find UCF101_RGB_S* -name test_nc25_1.log |grep vgg16pt_sdsel_ --color=never`
+#echo 'UCF-101 vgg16_s' && grep "MEAN TOP" `find UCF101_RGB_S* -name test_nc25_1.log |grep vgg16pt_s_ --color=never`
+#echo 'UCF-101 vgg16_d' && grep "MEAN TOP" `find UCF101_RGB_S* -name test_nc25_1.log |grep vgg16pt_d_ --color=never`
+#echo 'UCF-101 vgg16_sdsel' && grep "MEAN TOP" `find UCF101_RGB_S* -name test_nc25_1.log |grep vgg16pt_sdsel_ --color=never`
 
-#echo 'UCF MEAN CLASS'
-#echo 'UCF-101 vggm_s' && grep "MEAN CLASS" `find UCF101_RGB_S* -name test_nc25_1.log |grep vggmpt_s_ --color=never`
-#echo 'UCF-101 vggm_d' && grep "MEAN CLASS" `find UCF101_RGB_S* -name test_nc25_1.log |grep vggmpt_d_ --color=never`
-#echo 'UCF-101 vggm_sdsel' && grep "MEAN CLASS" `find UCF101_RGB_S* -name test_nc25_1.log |grep vggmpt_sdsel_ --color=never`
+echo 'UCF MEAN CLASS'
+echo 'UCF-101 vggm_s' && grep "MEAN CLASS" `find UCF101_RGB_S* -name test_nc25_1.log |grep vggmpt_s_ --color=never`
+echo 'UCF-101 vggm_d' && grep "MEAN CLASS" `find UCF101_RGB_S* -name test_nc25_1.log |grep vggmpt_d_ --color=never`
+echo 'UCF-101 vggm_sdsel' && grep "MEAN CLASS" `find UCF101_RGB_S* -name test_nc25_1.log |grep vggmpt_sdsel_ --color=never`
 
-#echo 'UCF-101 vgg16_s' && grep "MEAN CLASS" `find UCF101_RGB_S* -name test_nc25_1.log |grep vgg16pt_s_ --color=never`
-#echo 'UCF-101 vgg16_d' && grep "MEAN CLASS" `find UCF101_RGB_S* -name test_nc25_1.log |grep vgg16pt_d_ --color=never`
-#echo 'UCF-101 vgg16_sdsel' && grep "MEAN CLASS" `find UCF101_RGB_S* -name test_nc25_1.log |grep vgg16pt_sdsel_ --color=never`
+echo 'UCF-101 vgg16_s' && grep "MEAN CLASS" `find UCF101_RGB_S* -name test_nc25_1.log |grep vgg16pt_s_ --color=never`
+echo 'UCF-101 vgg16_d' && grep "MEAN CLASS" `find UCF101_RGB_S* -name test_nc25_1.log |grep vgg16pt_d_ --color=never`
+echo 'UCF-101 vgg16_sdsel' && grep "MEAN CLASS" `find UCF101_RGB_S* -name test_nc25_1.log |grep vgg16pt_sdsel_ --color=never`
 
 echo 'HMDB MEAN TOP1'
 echo 'HMDB51 vggm_s' && grep "MEAN TOP" `find HMDB51_RGB_S* -name test_nc25_1.log |grep vggmpt_s_ --color=never`
